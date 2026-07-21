4c5138a6c89340805e8323a44b05b4c2758b7ff8
Iqra Tai <iqratai38@gmail.com>
2026-07-21 17:07:04 +0530

    feat(products): fetch and display products from DummyJSON API

--- shopping/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ shopping/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,57 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
 import './FeaturedProducts.css'
 
 function FeaturedProducts() {
+  
+  const [products, setProducts] = useState([]);
+
+  useEffect( ()=> {
+    fetch("https://dummyjson.com/products?limit=194")
+    .then((response) => response.json() )
+    .then( (data) => {
+      //console.log(data)
+      //console.log(data.products)
+
+      setProducts(data.products)
+    } )
+  }, [] );
+
+  console.log("products:",products);
+  console.log("Lenght:",products.length);
   return (
    <section className="featured-products">
 
       <h2>Featured Products</h2>
 
       <div className="product-grid">
+        {products.map( (product) => (
+          <div className="product-card" key={product.id} >
+            <div className="product-image">
+              <img src={product.thumbnail} alt={product.title} />
+            </div>
 
-        <div className="product-card">
-          <div className="product-image">
-            <p>Product Image</p>
-          </div>
+            <h3>{product.title}</h3>
+            <p>{product.price}</p>
 
-          <h3>Product Title</h3>
-          <p>$99.99</p>
-
-          <button>View Details</button>
+            <button>View Details</button>
         </div>
+        ) )}
 
-        <div className="product-card">
-          <div className="product-image">
-            <p>Product Image</p>
-          </div>
-
-          <h3>Product Title</h3>
-          <p>$99.99</p>
-
-          <button>View Details</button>
-        </div>
-
-        <div className="product-card">
-          <div className="product-image">
-            <p>Product Image</p>
-          </div>
-
-          <h3>Product Title</h3>
-          <p>$99.99</p>
-
-          <button>View Details</button>
-        </div>
-
-        <div className="product-card">
-          <div className="product-image">
-            <p>Product Image</p>
-          </div>
-
-          <h3>Product Title</h3>
-          <p>$99.99</p>
-
-          <button>View Details</button>
-        </div>
 
       </div>
 
